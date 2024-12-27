82f2b046afb9c2d85822eccaf6528ccfa224f78a
shy7331 <gart.25@mail.ru>
2024-12-27 16:48:58 +0300

    добавлен модуль организатора OrganizerPage

---
+++ Tournament_422_Gabdrakhmanov/Pages/CreateTournamentPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Tournament_422_Gabdrakhmanov.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для CreateTournamentPage.xaml
+    /// </summary>
+    public partial class CreateTournamentPage : Page
+    {
+        public CreateTournamentPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- Tournament_422_Gabdrakhmanov/Pages/OrganizerPage.xaml.cs
+++ Tournament_422_Gabdrakhmanov/Pages/OrganizerPage.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Tournament_422_Gabdrakhmanov.Components;
 
 namespace Tournament_422_Gabdrakhmanov.Pages
 {
@@ -20,9 +21,36 @@ namespace Tournament_422_Gabdrakhmanov.Pages
     /// </summary>
     public partial class OrganizerPage : Page
     {
+        private TournamentDB_422_GabdraxmanovEntities db = new TournamentDB_422_GabdraxmanovEntities();
+
         public OrganizerPage()
         {
             InitializeComponent();
+            LoadTournaments();
+            LoadMatches();
+            LoadParticipants();
+        }
+
+        private void LoadTournaments()
+        {
+            TournamentsDataGrid.ItemsSource = db.Tournaments.ToList();
+        }
+
+        private void LoadMatches()
+        {
+            MatchesDataGrid.ItemsSource = db.Matches.ToList();
+        }
+
+        private void LoadParticipants()
+        {
+            ParticipantsDataGrid.ItemsSource = db.Player.ToList();
+        }
+
+        private void CreateTournamentButton_Click(object sender, RoutedEventArgs e)
+        {
+           
+            NavigationService.Navigate(new CreateTournamentPage());
         }
     }
 }
+
